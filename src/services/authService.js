b2b09564bc8017fe1518d11de18a31aec8d2da46
Nguyen Ngoc Duc <55783340+nnduc2207@users.noreply.github.com>
2022-01-17 15:50:14 +0700

    Create fetchAPILogin function

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ import * as Notifications from '../utils/notifications'
 
 
 const fetchLogin = (usernameToFetch, passwordToFetch) => {
-  return fetch(process.env.REACT_APP_API_URL+'/api/login', {
+  return fetch(process.env.REACT_APP_API_URL+'/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -33,43 +33,7 @@ const fetchLogin = (usernameToFetch, passwordToFetch) => {
   })
 }
 
-const fetchRegister = (username, fullname, email, password) => {
-  return fetch(process.env.REACT_APP_API_URL+'/api/user', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      username: username,
-      name: fullname,
-      email: email,
-      password: password
-    })
-  })
-  .then((res) => {
-    //console.log('res: ', res)
-    if (res.ok)
-      return res.json().then((result) => {
-        //console.log('result: ', result)
-        if (!result)
-          return { error: Notifications.API_RETURN_NULL_RESULT}
-        else if (result.errors)
-          return { error: result.errors}
-        else
-          return { data: result }
-      })
-    else
-      return { error: Notifications.REGISTER_FAILED }
-  })
-  .catch((error) => {
-    //console.log('error: ', error)
-    return { error: error }
-  })
-}
-
-
 export {
-  fetchLogin,
-  fetchRegister
+  fetchLogin
 }
 
