11a37794e7f95e6598d63bc19c75909a2594fc5a
Nguyen Ngoc Duc <55783340+nnduc2207@users.noreply.github.com>
2022-01-18 07:48:05 +0700

    Navigate to login page when token expired

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 import AuthContext from '../contexts/authContext'
+import { fetchGetAdmin } from "../services/adminService";
 
 
 
@@ -20,11 +21,23 @@ const NonAuthRequired = ({ children }) => {
 }
 
 const AuthRequired = ({ children }) => {
-  const { currentUser } = useContext(AuthContext);
+  const { currentUser, setCurrentUser } = useContext(AuthContext);
   const location = useLocation()
 
+  useEffect(()=>{
+    const checkIsLoginExpired = async () => {
+      if (currentUser) {
+        const user = await fetchGetAdmin(currentUser.token, currentUser.user._id);
+        if (user.error) {
+          setCurrentUser(null);
+        }
+      }
+    }
+    checkIsLoginExpired()
+  },[])
+
   if (!currentUser) {
-    return <Navigate to='/login' state={{ from: location}} />
+    return <Navigate to='/login' />
   }
 
   return children
